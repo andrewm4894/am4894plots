4f4860efce1dc33519f2c5381d00adf9a846c432
Maguire, Andrew <andrew_maguire@bmc.com>
2019-11-14 14:04:53 +0000

    updated docs to sphinx-rtd-theme

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
+import sphinx-rtd-theme
+
 import am4894plots
 
 # -- General configuration ---------------------------------------------
@@ -32,7 +34,7 @@ import am4894plots
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx-rtd-theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -84,7 +86,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = 'sphinx-rtd-theme'
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
