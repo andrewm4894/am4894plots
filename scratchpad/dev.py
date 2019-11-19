6fe86c1f65cf7627831f6981bd1457b01cc073a1
Maguire, Andrew <andrew_maguire@bmc.com>
2019-11-19 14:58:56 +0000

    adding plot animation dev stuff

--- scratchpad/dev.py
+++ scratchpad/dev.py
@@ -1,33 +1,45 @@
-#%%
 
-from am4894pd.utils import df_dummy_ts
-from am4894plots.lines.bk import plot_lines, plot_lines_grid
+# taken from here: https://stackoverflow.com/a/17903145/1919374
 
-#%%
-
-df = df_dummy_ts(n_cols=2)
-print(df.shape)
-
-#%%
-
-p = plot_lines_grid(df, out_path='scratchpad/plot.html', return_p=True, show_p=False)
-
-#%%
-
-print(type(p))
-
-#%%
-
-#dir(p)
-#len(p.children[0].to_json(include_defaults=True)['children'])
-p.children[0].to_json(include_defaults=True)['children'][0][0]['type']
+import matplotlib.pylab as plt
+import matplotlib.animation as animation
+import numpy as np
 
 
-#%%
+n_data = 100
+n_cols = 2
+im_list = []
 
-print(p.l)
+data = np.random.rand(n_data, 10, 10)
+
+fig, axes = plt.subplots(n_cols)
+
+repeat_length = (np.shape(image)[0]+1)/4
+for n in range(n_cols):
+    axes[n].set_xlim([0, repeat_length])
+    axes[n].autoscale_view()
+    axes[n].set_ylim([np.amin(data[:, 5, 5]), np.amax(data[:, 5, 5])])
+
+im1, = axes[0].plot([], [], color=(0, 0, 1))
+im2, = axes[1].plot([], [], color=(0, 0, 1))
 
 
-#%%
+def func(n):
+    im1.set_xdata(np.arange(n))
+    im1.set_ydata(image[0:n, 5, 5])
+    im2.set_xdata(np.arange(n))
+    im2.set_ydata(image[0:n, 5, 5])
+    if n > repeat_length:
+        axes[0].set_xlim(n-repeat_length, n)
+        axes[1].set_xlim(n - repeat_length, n)
+    else:
+        axes[0].set_xlim(0, repeat_length)
+        axes[1].set_xlim(0, repeat_length)
+    return im1, im2
 
-#plot_lines_grid(df, h=200, out_path='scratchpad/plot.html')
+ani = animation.FuncAnimation(fig, func, frames=image.shape[0], interval=30, blit=False)
+
+ani.save('tmp/animation.mp4')
+
+plt.show()
+
