5072a991166901e71c64c4f4bb7d960fa3d6fde7
Maguire, Andrew <andrew_maguire@bmc.com>
2019-11-20 21:59:33 +0000

    adding plots namespace for default plots

---
+++ am4894plots/plots.py
@@ -0,0 +1,3 @@
+from am4894plots.lines.plotly import plot_lines, plot_lines_grid
+from am4894plots.dist.plotly import plot_boxes
+
--- scratchpad/dev.py
+++ scratchpad/dev.py
@@ -2,14 +2,18 @@
 
 import pandas as pd
 from am4894pd.utils import df_dummy_ts
-from am4894plots.dist.plotly import plot_boxes
+from am4894plots.plots import plot_lines, plot_boxes
 
 
 df = df_dummy_ts(n_cols=10, freq='1min')
 print(df.shape)
 
-plot_boxes(df, n_cols=5)
+#%%
 
+plot_lines(df)
 
+#%%
+
+plot_boxes(df)
 
 
