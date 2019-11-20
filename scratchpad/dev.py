0c923c8933221ebf44928c234247a58dfdd6a434
Maguire, Andrew <andrew_maguire@bmc.com>
2019-11-20 18:01:36 +0000

    adding some dev for boxplots

--- scratchpad/dev.py
+++ scratchpad/dev.py
@@ -1,11 +1,30 @@
 #%%
+import math
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 from am4894pd.utils import df_dummy_ts
-from am4894plots.dist.bokeh import plot_hists
 
-df = df_dummy_ts(n_cols=2, freq='1min')
+df = df_dummy_ts(n_cols=3, freq='1min')
 print(df.shape)
 
+cols = df.columns
+cols_len = len(cols)
+n_cols = 2
+if cols_len == n_cols:
+    n_cols = n_cols - 1
+n_rows = math.ceil(cols_len / n_cols)
+figsize = (10, 8)
+fig, axes = plt.subplots(n_rows, n_cols, figsize=figsize)
 
-plot_hists(df, out_path='tmp/plot.html')
+print(n_rows, n_cols)
 
+for i, col in enumerate(cols):
+    ax_row = i // n_cols
+    ax_col = i % n_cols
+    ax_curr = axes[ax_row, ax_col]
+    sns.boxplot(y=col, data=df, ax=ax_curr)
+
+plt.show()
+
+#%%
